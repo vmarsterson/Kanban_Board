9ae82a77e2e7462c5ea90d46dd178cda64399fed
jack272hughes <jack272hughes@gmail.com>
2019-08-30 12:13:12 +0100

    Added routes for POST and GET forms in index

--- Server.rb
+++ Server.rb
@@ -4,14 +4,18 @@ require_relative './lib/task_tracker.rb'
 
 class Server < Sinatra::Base
     get '/' do
-        erb :index
+        erb :index, locals: {kanban_boards: Kanban_board.all}
     end
 
-    post '/kanbanBoard/:id' do
-        erb :kanban_display, locals: {kanban_boards: Kanban_board.all}
+    post '/kanbanBoard' do
+        kanbanTitle = params["boardTitle"]
+        Kanban_board.create(title: kanbanTitle, status_column: "todo")
+        id = Kanban_board.all.last.id
+        erb :kanban_display, locals: {kanban_board: Kanban_board.find(id)}
     end
 
-    get '/kanbanBoard/:id' do
-        erb :kanban_display, locals: {kanban_boards: Kanban_board.all}
+    get '/kanbanBoard' do
+        id = params["boardID"]
+        erb :kanban_display, locals: {kanban_board: Kanban_board.find(id)}
     end
 end
